de9fb83935bb5668dcfe22d9818ba1f4cb3d7cea
parthu <parthumajjiga2004@gmail.com>
2023-10-15 20:17:50 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
-import {Calc} from './components/compute.js';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+const Calculator = () => {
+  const [input, setInput] = useState("");
+  const [result, setResult] = useState("");
+
+  const handleButtonClick = (value) => {
+    setInput(input + value);
+  };
+
+  const clearInput = () => {
+    setInput("");
+    setResult("");
+  };
+
+  const calculateResult = () => {
+    try {
+      setResult(eval(input).toString());
+    } catch (error) {
+      setResult("Error");
+    }
+  };
+
   return (
-    <div className="App">
-      <Calc/>
+    <div className="calculator">
+      <div className="display">
+        <input type="text" value={input} readOnly />
+      </div>
+      <div className="buttons">
+        <button onClick={() => handleButtonClick("1")}>1</button>
+        <button onClick={() => handleButtonClick("2")}>2</button>
+        <button onClick={() => handleButtonClick("3")}>3</button>
+        <button onClick={() => handleButtonClick("+")}>+</button>
+        <button onClick={() => handleButtonClick("4")}>4</button>
+        <button onClick={() => handleButtonClick("5")}>5</button>
+        <button onClick={() => handleButtonClick("6")}>6</button>
+        <button onClick={() => handleButtonClick("-")}>-</button>
+        <button onClick={() => handleButtonClick("7")}>7</button>
+        <button onClick={() => handleButtonClick("8")}>8</button>
+        <button onClick={() => handleButtonClick("9")}>9</button>
+        <button onClick={() => handleButtonClick("*")}>*</button>
+        <button onClick={() => handleButtonClick("0")}>0</button>
+        <button onClick={clearInput}>C</button>
+        <button onClick={calculateResult}>=</button>
+        <button onClick={() => handleButtonClick("/")}>/</button>
+      </div>
+      <div className="result">
+        <span>{result}</span>
+      </div>
     </div>
   );
-}
+};
 
-export default App;
+export default Calculator;
